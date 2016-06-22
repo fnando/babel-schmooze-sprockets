8b034c5c6ccc5fa5ea20b4fdc4f5694e8a1dd637
Lucas Mazza <lucasmazza@users.noreply.github.com>
2016-06-22 18:53:59 -0300

    Stop appending to the Rails assets load path. (#3)
    As all shims/polyfills are required from the `app/assets/javascripts`, we don't
    need to add the current `node_modules` directory to the load path.

--- lib/babel-schmooze-sprockets/engine.rb
+++ lib/babel-schmooze-sprockets/engine.rb
@@ -1,7 +1,4 @@
 module BabelSchmoozeSprockets
   class Railtie < Rails::Engine
-    initializer "babel-schmooze-sprockets" do
-      config.assets.paths << File.expand_path("#{__dir__}/../../node_modules")
-    end
   end
 end
